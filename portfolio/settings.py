1cfa03a8b384451c70879a9a3731b4a6a707e90e
Mubarak Lawal <mubaraklawal52@gmail.com>
2025-07-07 21:47:28 +0200

    update favicon

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -115,6 +115,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+
 CSRF_TRUSTED_ORIGINS=['https://*.fly.dev']
 
 
